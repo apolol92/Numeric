faef818035636cc43e970309cac26c5d690247f3
apolol92 <apolol92@web.de>
2015-04-17 10:48:48 +0200

    Revert "Added and edited some methods"
    This reverts commit ca888214f3861f637f5610a09eb34f3d23f36472.

--- Analysis/newton_method.m
+++ Analysis/newton_method.m
@@ -6,10 +6,10 @@ function [ xn ] = newton_method( f, x0, h, steps )
 xn = x0;
 i = 0;
 while i < steps
-    v =  double(f(xn+h))-double(f(xn));
+    v =  f(xn+h)-f(xn);
     derivation = v/h;
-    xn = xn - double(f(xn))/derivation;
+    xn = xn - f(xn)/derivation;
     i=i+1;
-output = xn;
+output = xn
 end
 
